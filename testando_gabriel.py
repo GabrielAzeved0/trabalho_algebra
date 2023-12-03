e26bb696deaf453f39cf5df27f609015f0e6f5b1
DarkBig <gabriel.azevedobc@gmail.com>
2023-12-03 15:31:03 -0300

    funcao de jumps

--- testando_gabriel.py
+++ testando_gabriel.py
@@ -1,12 +1,12 @@
 import table_functions
-import input_functions
-
 
 'matriz de str'
 texto_usuario = 'felipe ta atrasado'
 
-'matriz de int'
 matriz = table_functions.text_to_table(texto_usuario)
+table_functions.table_order(matriz)
+matriz = table_functions.format_table(matriz)
+print(matriz)
 table_functions.jump_columns(matriz)
 
 
