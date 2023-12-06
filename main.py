e79f7df666e6737b6b0207df7c6d99f1a1b7fd4b
Gabriel <gabriel.azevedobc@gmail.com>
2023-12-06 20:46:52 -0300

    a

---
+++ main.py
@@ -0,0 +1,21 @@
+import table_functions
+import random
+
+menu = """
+-----------------------MENU-----------------------
+
+1 - CRIPTOGRAFAR
+2 - DESCRIPTOGRAFAR
+
+Operação Desejada: 
+"""
+
+op = input()
+
+if op == 1:
+    pass
+if op == 2:
+    pass
+
+
+
